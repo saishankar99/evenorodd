010caea73a620220f43bcf7da5b3f7f193cf0d5f
saishankar99 <53267402+saishankar99@users.noreply.github.com>
2019-07-27 14:55:18 +0530

    Delete lRGESTNUMBER.py

--- lRGESTNUMBER.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter first number="))
-b=int(input("enter second number="))
-c=int(input("enter third number="))
-if a>b and a>c:
-    print(a, "is the largest number")
-elif c>b:
-    print(c, "is the largest number")
-else:
-    print(b, "is the largest number")
