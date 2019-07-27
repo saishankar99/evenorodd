606c7e3bb81d0a06ae67dd592ba73164e115a199
saishankar99 <53267402+saishankar99@users.noreply.github.com>
2019-07-27 10:50:48 +0530

    Delete ALPHEBETORNOT.py

--- ALPHEBETORNOT.py
+++
@@ -1,5 +0,0 @@
-a=input("enter an alphabet")
-if a in ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","x","v","w","y","z","A","B","C","D","E","F","G","H","I","J","k","L","M","N","O","Q","P","R","S","T","U","V","W","X","Y","Z"):
-    print("alphabet")
-else:
-    print("no")
