bab8ae34523097b34e0078f601e7b660f7f0b3e4
saishankar99 <53267402+saishankar99@users.noreply.github.com>
2019-08-16 19:53:27 +0530

    sumofkint.py

---
+++ sumofkint.py
@@ -0,0 +1,12 @@
+N=int(input("enter a value for N: "))
+K=int(input("enter a value for K: "))
+print()
+print("The array of N integers is:")
+for i in range(1,N+1):
+    N=N-1
+    print(i)
+    for i in range(1,K+1):
+        i=(i*(i+1))//2
+print()
+print("sum of first K integers is:")
+print(i)
